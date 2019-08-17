c39bede772b6623b9b4b5b5ba07142fe790086e0
Angelina Yurkova <anhelina.yurkova@gmail.com>
2019-08-17 16:39:04 -0400

    git practice

--- src/test/java/com/vytrack/step_definitions/ContactStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/ContactStepDefs.java
@@ -1,6 +1,5 @@
 package com.vytrack.step_definitions;
 
-
 import com.vytrack.pages.ContactsPage;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.pages.ViewContactPage;
@@ -26,16 +25,38 @@ public class ContactStepDefs {
         String actual = new ViewContactPage().email.getText();
         Assert.assertEquals("Customer email did not match", expectedEmail, actual);
     }
+
     @Given("I login using these credentials")
     public void i_login_using_these_credentials(Map<String, String> loginInfo) {
         System.out.println(loginInfo);
-        String username = loginInfo.get("Yourusername");
-        String password = loginInfo.get("Yourpassword");
-        System.out.println("username = "+ username);
-        System.out.println("password = "+ password);
-        new LoginPage().login(username,password);
+        String username = loginInfo.get("username");
+        String password = loginInfo.get("password");
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+        new LoginPage().login(username, password);
+    }
+
+    @Then("contact creation details should display correctly")
+    public void contact_creation_details_should_display_correctly(Map<String, String> userInfo) {
+        System.out.println(userInfo);
+
+        String expectedCreateDate = userInfo.get("Created At");
+        String expectedOwner = userInfo.get("Owner");
+        System.out.println("expectedCreateDate = " + expectedCreateDate);
+        System.out.println("expectedOwner = " + expectedOwner);
+
+        ViewContactPage viewContactPage = new ViewContactPage();
+        String actualCreateDate = viewContactPage.createdAt.getText();
+        String actualOwner = viewContactPage.owner.getText();
+
+        System.out.println("actualCreateDate = " + actualCreateDate);
+        System.out.println("actualOwner = " + actualOwner);
+
+        Assert.assertTrue(actualCreateDate.contains(expectedCreateDate));
+        Assert.assertEquals(expectedOwner, actualOwner);
 
     }
+
 
 
 }
